38e101aa712f6444764180b56b47593c66fcdd66
Jair Gonzalez <jjglezp@gmail.com>
2016-07-13 21:17:31 +0000

    Internet of Things 101: 5.4.2 Protocols: Project: MQTT Subscribe
    Aubacribe to "IoT101/DataActuator" topic through MQTT Protocol using
    test.mosquitto.org server to change the status of Data Actuator
    
    Signed-off-by: Jair Gonzalez <jjglezp@gmail.com>

--- InternetOfThings101/main.py
+++ InternetOfThings101/main.py
@@ -1,17 +1,45 @@
 #!/usr/bin/python
 
+import paho.mqtt.client as paho
 import psutil
 import signal
 import sys
 import time
 
-def functionDataActuator():
-    print "Data Actuator"
+from threading import Thread
+
+def functionDataActuator(status):
+    print "Data Actuator Status %s" % status
+
+def functionDataActuatorMqttOnMessage(mosq, obj, msg):
+    print "Data Sensor Mqtt Subscribe Message!"
+    functionDataActuator(msg.payload)
+
+def functionDataActuatorMqttSubscribe():
+    mqttclient = paho.Client()
+    mqttclient.on_message = functionDataActuatorMqttOnMessage
+    mqttclient.connect("test.mosquitto.org", 1883, 60)
+    mqttclient.subscribe("IoT101/DataActuator", 0)
+    while mqttclient.loop() == 0:
+        pass
 
 def functionDataSensor():
     netdata = psutil.net_io_counters()
     data = netdata.packets_sent + netdata.packets_recv
     return data
+
+def functionDataSensorMqttOnPublish(mosq, obj, msg):
+    print "Data Sensor Mqtt Published!"
+
+def functionDataSensorMqttPublish():
+    mqttclient = paho.Client()
+    mqttclient.on_publish = functionDataSensorMqttOnPublish
+    mqttclient.connect("test.mosquitto.org", 1883, 60)
+    while True:
+        data = functionDataSensor()
+        topic = "IoT101/DataSensor"
+        mqttclient.publish(topic, data)
+        time.sleep(1)
 
 def functionSignalHandler(signal, frame):
     sys.exit(0)
@@ -19,6 +47,12 @@ def functionSignalHandler(signal, frame):
 if __name__ == '__main__':
 
     signal.signal(signal.SIGINT, functionSignalHandler)
+
+    threadmqttpublish = Thread(target=functionDataSensorMqttPublish)
+    threadmqttpublish.start()
+
+    threadmqttsubscribe = Thread(target=functionDataActuatorMqttSubscribe)
+    threadmqttsubscribe.start()
 
     while True:
         print "Hello Internet of Things 101"
